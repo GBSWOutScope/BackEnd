4e4f9dd604e663f7e44644be45bbd37e70640c3a
Hanavi999 <chanhyeon777@gmail.com>
2023-04-17 09:40:07 +0900

    .

---
+++ OutScope/src/main/java/com/example/outscope/OutScopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.outscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutScopeApplication.class, args);
+    }
+
+}
